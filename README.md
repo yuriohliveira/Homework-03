197f99c9c520ca11f2ec5905967d19633e912cfa
yuriohliveira <61686424+yuriohliveira@users.noreply.github.com>
2020-03-22 18:57:33 -0700

    general polishing

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# Homework-03
+Homework 03 - Password Generator
+
+Motivation: Making a random password generator, obeying specific rules and giving specific options for the user to choose from. Taking the utility point of view, this activity is extremely useful, for all sorts of situations.
+
+In this case specifically, is just a website for personal use, letting the user choose from 4 different types of characters and a size between 8 and 128 for his random password. It has good flexibility and a strong method, making it a reliable tool for anyone that wants a new password.
+
+From the point of view of a developer, was a great way to work with javascript and manipulate the random number function and mixing it with strings. Since the 'random' factor is a primal concept of security, this covers a great topic which developers must face on a daily basis, not only for user passwords, but security keys, which are generated all the time on the internet.
+
+
+
+A few features:
+
+- simple and userfriendly interface
+- reliable method through javascript
--- script.js
+++ script.js
@@ -1,70 +1,85 @@
-generate.onclick = function clickbutton() {
+// setting a global variable as empty string to store the password and another for total available characters
+  var password = "";
+  var totalsize = "";
 
+// setting a click function for the GENERATE PASSWORD button
+  generate.onclick = function clickbutton() {
+
+// asking the user to choose a size between 8 and 128 for his random password
   var passwordlength = prompt("Choose a number of characters between 8 and 128");
 
     if (passwordlength < 8 || passwordlength > 128){
-      alert("invalid number modafoka");
+      alert("invalid number");
       return;
-    } else {}
+    }
 
-  alert("You must choose at least one criteria for your password");
+// asking the user to choose at least one from the 4 possible criterias, then passing 4 boolean variables and the password length
+// through the function
+
+    alert("You must choose at least one criteria for your password");
     
-  var promptlowerlet = confirm("Do you want to include lowercase letters in your password?");
-  var promptupperlet = confirm("Do you want to include uppercase letters in your password?");
-  var promptnumber = confirm("Do you want to include numbers in your password?");
-  var promptspecialchar = confirm("Do you want to include special characters in your password?");
+    var Lower = confirm("Do you want to include lowercase letters in your password?");
+    var Upper = confirm("Do you want to include uppercase letters in your password?");
+    var Numbers = confirm("Do you want to include numbers in your password?");
+    var specialChar = confirm("Do you want to include special characters in your password?");
 
-  if (promptlowerlet === false && promptupperlet === false && promptnumber === false && promptspecialchar === false){
-    alert("Try again dude")
-    return;
-  } else {}
+// condition for moving on (at least 1 criteria must be set)
 
-     generatePassword(promptlowerlet, promptupperlet, promptnumber, promptspecialchar, passwordlength);
-    
+    if (Lower === false && Upper === false && Numbers === false && specialChar === false){
+      alert("Try again dude")
+      return;
+    } else {
 
+// calls generatePassword function
+      generatePassword(Lower, Upper, Numbers, specialChar, passwordlength);
+    }
 
-}
+  }
 
-function generatePassword(promptlowerlet, promptupperlet, promptnumber, promptspecialchar, passwordlength) {
+// function to generate the password, using previous parameters
+  function generatePassword(Lower, Upper, Numbers, specialChar, passwordlength) {
+
+// resets password to empty string in case the user uses it more than once
+    password = "";
+
+// sets all possible characters in 4 strings, and sets them to empty if the user chooses not to include them
 
     var lower = "abcdefghijklmnopqrstuvwxyz";
-          if( promptlowerlet === false) {
+          if( Lower === false) {
             lower = "";
-          } else {}
-        upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-          if( promptupperlet === false) {
-            upper = "";
-          } else {}
-        numbers = "0123456789";
-          if( promptnumber === false) {
-            numbers = "";
-          } else {}
-        special = "!#$%&'\"()*+,-./:;<=>?@[\]^_`{|}~";
-          if( promptspecialchar === false) {
-            special = "";
-          } else {}
+          }
 
-      passwordready = "";
+        upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+          if( Upper === false) {
+            upper = "";
+          }
+
+        numbers = "0123456789";
+          if( Numbers === false) {
+            numbers = "";
+          }
+
+        special = "!#$%&'\"()*+,-./:;<=>?@[\]^_`{|}~";
+          if( specialChar === false) {
+            special = "";
+          }
+
+// string containing the total amount of characters the user chooses to include
       totalsize = lower + upper + numbers + special;
 
+// takes a random number, multiplies by totalsize length, makes it integer and makes it an index for totalsize string, then adds to the
+// password
   for (var i = 0; i < passwordlength; ++i) {
-    passwordready += totalsize.charAt(Math.floor(Math.random() * totalsize.length));
+    password += totalsize.charAt(Math.floor(Math.random() * totalsize.length));
   }
-  console.log("Your password is: " + passwordready);
-  return passwordready;
-}
-
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+  
+// calls function to write password on the website
+  writePassword();
+  }
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  function writePassword() {
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
 
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  }
